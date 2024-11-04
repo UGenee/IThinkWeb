4f055d9941b58d297e0287f0f378133e90298da2
UGenee <ephraimbonifacio312@gmail.com>
2024-11-04 22:23:05 +0800

    Added_template_forNavand_Added_contacts

--- index/IThink.js
+++ index/IThink.js
@@ -1,3 +1,19 @@
+// NAVIGATION BAR
+function loadNav() {
+    fetch('nav.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('nav-container').innerHTML = data;
+        });
+}
+    window.onload = loadNav;
+
+document.querySelector('.dropdown-button').addEventListener('click', function() {
+    const nav = document.querySelector('.nav');
+    nav.classList.toggle('show');
+});
+
+// HOME PAGE
 function start() { // Container Button very start of the home page
     window.open("/index/test.html");
 }
